09fe91f91c57f918013e7f27588a727d036d8678
Ronald yeung <ryeung@wizlink.jp>
2024-01-21 00:03:32 +0800

    update

--- index.php
+++ index.php
@@ -2,20 +2,34 @@
 <html>
 <head>
 <title>QUIZ GAME</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 </head>
 <body>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <v-container>
+            <v-card>
+            {{message}}
+            </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
 
-<div id="app">
-  {{ message }}
-</div>
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+  <script>
+    new Vue({
+      el: '#app',
+      vuetify: new Vuetify(),
+     data: {
+        message: 'QUIZ GAME'
+    }
+    })
+  </script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/vue@2.7.16/dist/vue.js"></script>
-<script>
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'QUIZ GAME'
-  }
-})
-</script>
-</html> 
+</html>
