64d1c755a24cbb4dfbfcb1b987138b0449f544f7
TakuryuYo <lunglung1234143@gmail.com>
2024-01-21 18:46:48 +0900

    update 1/21

--- common.php
+++ common.php
@@ -12,5 +12,7 @@
     $common_js = <<<EOF
     <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
 EOF;
 ?>
