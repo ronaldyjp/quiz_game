e36ec10e0eb79911be55ce73bd1531e388189fde
Ronald yeung <ryeung@wizlink.jp>
2024-02-12 21:57:25 +0800

    update

--- api/report.php
+++ api/report.php
@@ -3,23 +3,15 @@
     $event_id = $_GET['event_id'];
     $sql =<<<EOF
 
-    select 
-    FROM q0012_event e
-    inner join q0013_event_game_rel r
-     on e.id = r.q0012_id
-    inner join q0022_question_show_record s
-     on s.q0012_id = e.id 
-    inner join q0011_game g
-     on s.q0021_id = g.id
+    select u.id as user_id, u.a_name as user_name, t.team_id, t.team_name, t.team_school, sum(q.point) as point
+    from q0022_question_show_record s
     inner join q0021_question q
-     on q.q0011_id = g.id
-    
-    
-     SELECT 
-     a.id as user_id, 
-     t.team_id, t.team_name, t.team_school
-     from jstudy_t0010_apply a  
-     inner join
+        on q.id = s.q0021_id    
+    inner join q0004_user_answer a
+        on a.q0021_id = q.id
+    inner join jstudy_t0010_apply u
+        on a.q0001_id = u.id
+    left join
       (select a.id as team_id , a.a_email as email, t.name as team_name, t.school as team_school, t.q0012_id as event_id
          from jstudy_t0010_apply a
          inner join q0002_team t
@@ -28,7 +20,7 @@
      ) t
      on t.team_id = a.ref_apply_id
      where t.event_id = 1
-
+    group by t.team_id
      
 EOF;    
 
--- doc/create_table_and_sample_data.sql
+++ doc/create_table_and_sample_data.sql
@@ -77,7 +77,19 @@ CREATE TABLE IF NOT EXISTS `jstudy_t0010_apply` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=21640 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.jstudy_t0010_apply: 9 rows のデータをダンプしています
+/*!40000 ALTER TABLE `jstudy_t0010_apply` DISABLE KEYS */;
+INSERT INTO `jstudy_t0010_apply` (`id`, `a_num`, `a_type`, `ref_id`, `ref_apply_id`, `event_name`, `domain`, `a_name`, `a_kanji`, `a_sex`, `a_address`, `a_area`, `a_tel`, `a_whatsapp`, `a_email`, `a_pos`, `a_dep`, `a_cm`, `e_level`, `j_level`, `u_level`, `a_0`, `a_1`, `a_2`, `a_3`, `a_4`, `a_5`, `c_0`, `c_1`, `c_2`, `c_3`, `c_4`, `c_5`, `created`, `modified`, `a_school`, `school_area`, `p_0`, `p_1`, `p_2`, `p_3`, `p_4`, `p_5`, `u_0`, `u_1`, `u_2`, `u_3`, `u_4`, `u_5`, `e_0`, `e_1`, `e_2`, `e_3`, `e_4`, `e_5`, `sel_docs`, `data_json`, `memo`) VALUES
+	(1, NULL, 'E', NULL, 1, NULL, NULL, 'user1_1', NULL, NULL, NULL, NULL, '0011', NULL, 'user1_1@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:01:58', NULL, 'school1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(2, NULL, 'E', NULL, 1, NULL, NULL, 'user1_2', NULL, NULL, NULL, NULL, '0012', NULL, 'user1_2@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:01:58', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(3, NULL, 'E', NULL, 1, NULL, NULL, 'user1_3', NULL, NULL, NULL, NULL, '0013', NULL, 'user1_3@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:01:59', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(4, NULL, 'E', NULL, 4, NULL, NULL, 'user1_4', NULL, NULL, NULL, NULL, '0014', NULL, 'user1_4@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:01', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(5, NULL, 'E', NULL, 4, NULL, NULL, 'user1_5', NULL, NULL, NULL, NULL, '0015', NULL, 'user1_5@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:04', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(8, NULL, 'E', NULL, NULL, NULL, NULL, 'user3_1', NULL, NULL, NULL, NULL, '0016', NULL, 'user3_1@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:07', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(6, NULL, 'E', NULL, 6, NULL, NULL, 'user2_1', NULL, NULL, NULL, NULL, '0021', NULL, 'user2_1@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:07', NULL, 'school2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(7, NULL, 'E', NULL, 6, NULL, NULL, 'user2_2', NULL, NULL, NULL, NULL, '0022', NULL, 'user2_2@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:07', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(9, NULL, 'E', NULL, NULL, NULL, NULL, 'user3_2', NULL, NULL, NULL, NULL, '0016', NULL, 'user3_2@asjas.org', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-01-19 11:02:07', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `jstudy_t0010_apply` ENABLE KEYS */;
 
 --  テーブル quiz_game.q0001_user の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0001_user` (
@@ -90,7 +102,16 @@ CREATE TABLE IF NOT EXISTS `q0001_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0001_user: ~8 rows (約) のデータをダンプしています
+INSERT INTO `q0001_user` (`id`, `q0002_id`, `name`, `email`, `phone`, `created_at`) VALUES
+	(1, 1, 'user1_1', 'user1_1@asjas.org', '0011', '2024-01-19 10:20:07'),
+	(2, 1, 'user1_2', 'user1_2@asjas.org', '0012', '2024-01-19 11:01:58'),
+	(3, 1, 'user1_3', 'user1_3@asjas.org', '0013', '2024-01-19 11:01:59'),
+	(4, 2, 'user1_4', 'user1_4@asjas.org', '0014', '2024-01-19 11:02:01'),
+	(5, 2, 'user1_5', 'user1_5@asjas.org', '0015', '2024-01-19 11:02:04'),
+	(6, 2, 'user1_6', 'user1_6@asjas.org', '0016', '2024-01-19 11:02:07'),
+	(7, 3, 'user2_1', 'user2_1@asjas.org', '0021', '2024-01-19 11:02:07'),
+	(8, 3, 'user2_2', 'user2_2@asjas.org', '0022', '2024-01-19 11:02:07');
 
 --  テーブル quiz_game.q0002_team の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0002_team` (
@@ -102,7 +123,11 @@ CREATE TABLE IF NOT EXISTS `q0002_team` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0002_team: ~3 rows (約) のデータをダンプしています
+INSERT INTO `q0002_team` (`id`, `q0012_id`, `leader_email`, `name`, `school`) VALUES
+	(1, 1, 'user1_1@asjas.org', 'school1_1', 'school1'),
+	(2, 1, 'user1_4@asjas.org', 'school1_2', 'school1'),
+	(3, 1, 'user2_1@asjas.org', 'school2', 'school2');
 
 --  テーブル quiz_game.q0004_user_answer の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0004_user_answer` (
@@ -116,7 +141,32 @@ CREATE TABLE IF NOT EXISTS `q0004_user_answer` (
   UNIQUE KEY `q0001_id_q0012_id_q0021_id` (`q0001_id`,`q0012_id`,`q0021_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0004_user_answer: ~24 rows (約) のデータをダンプしています
+INSERT INTO `q0004_user_answer` (`id`, `q0001_id`, `q0012_id`, `q0021_id`, `answer`, `created_at`) VALUES
+	(1, 1, 1, 1, 1, '2024-01-30 04:00:00'),
+	(2, 1, 1, 2, 2, '2024-01-30 05:00:00'),
+	(3, 1, 1, 3, 3, '2024-01-30 06:00:00'),
+	(4, 1, 1, 4, 4, '2024-01-30 07:00:00'),
+	(5, 2, 1, 1, 1, '2024-01-30 04:00:00'),
+	(6, 2, 1, 2, 2, '2024-01-30 05:00:00'),
+	(7, 2, 1, 3, 3, '2024-01-30 06:00:00'),
+	(8, 2, 1, 4, 4, '2024-01-30 07:00:00'),
+	(9, 4, 1, 1, 1, '2024-01-30 04:00:00'),
+	(10, 4, 1, 2, 2, '2024-01-30 05:00:00'),
+	(11, 4, 1, 3, 1, '2024-01-30 06:00:00'),
+	(12, 4, 1, 4, 1, '2024-01-30 07:00:00'),
+	(13, 6, 1, 1, 1, '2024-01-30 04:00:00'),
+	(14, 6, 1, 2, 1, '2024-01-30 05:00:00'),
+	(15, 6, 1, 3, 1, '2024-01-30 06:00:00'),
+	(16, 6, 1, 4, 1, '2024-01-30 07:00:00'),
+	(17, 8, 1, 1, 1, '2024-01-30 04:00:00'),
+	(18, 8, 1, 2, 2, '2024-01-30 05:00:00'),
+	(19, 8, 1, 3, 3, '2024-01-30 06:00:00'),
+	(20, 8, 1, 4, 4, '2024-01-30 07:00:00'),
+	(21, 9, 1, 1, 1, '2024-01-30 04:00:00'),
+	(22, 9, 1, 2, 2, '2024-01-30 05:00:00'),
+	(23, 9, 1, 3, 1, '2024-01-30 06:00:00'),
+	(24, 9, 1, 4, 1, '2024-01-30 07:00:00');
 
 --  テーブル quiz_game.q0011_game の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0011_game` (
@@ -125,7 +175,10 @@ CREATE TABLE IF NOT EXISTS `q0011_game` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0011_game: ~2 rows (約) のデータをダンプしています
+INSERT INTO `q0011_game` (`id`, `name`) VALUES
+	(1, 'Game1'),
+	(2, 'Game2');
 
 --  テーブル quiz_game.q0012_event の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0012_event` (
@@ -136,7 +189,9 @@ CREATE TABLE IF NOT EXISTS `q0012_event` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0012_event: ~1 rows (約) のデータをダンプしています
+INSERT INTO `q0012_event` (`id`, `name`, `event_date`, `city`) VALUES
+	(1, 'event1', '2024-02-12 00:00:00', 'hk');
 
 --  テーブル quiz_game.q0013_event_game_rel の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0013_event_game_rel` (
@@ -149,7 +204,10 @@ CREATE TABLE IF NOT EXISTS `q0013_event_game_rel` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0013_event_game_rel: ~2 rows (約) のデータをダンプしています
+INSERT INTO `q0013_event_game_rel` (`id`, `name`, `start_time`, `end_time`, `q0011_id`, `q0012_id`) VALUES
+	(1, '第一回合', '2024-01-30 12:00:00', '2024-01-30 13:00:00', 1, 1),
+	(2, '第二回合', '2024-01-30 13:00:00', '2024-01-30 14:00:00', 2, 1);
 
 --  テーブル quiz_game.q0021_question の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0021_question` (
@@ -166,7 +224,13 @@ CREATE TABLE IF NOT EXISTS `q0021_question` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0021_question: ~5 rows (約) のデータをダンプしています
+INSERT INTO `q0021_question` (`id`, `q0011_id`, `image`, `question`, `choices1`, `choices2`, `choices3`, `choices4`, `answer`, `view_order`) VALUES
+	(1, 1, 'q1.jpg', 'q1_1', 'c1_1', 'c1_2', 'c1_3', 'c1_4', 1, 1),
+	(2, 1, NULL, 'q2_1', 'c2_1', 'c2_2', 'c2_3', 'c2_4', 2, 2),
+	(3, 1, 'q3.jpg', 'q3_1', '', '', '', '', 3, 3),
+	(4, 1, NULL, 'q4_1', 'c4_1', 'c4_2', 'c4_3', 'c4_4', 4, 4),
+	(5, 1, 'q5.jpg', 'q5_1', 'c5_1', 'c5_2', 'c5_3', 'c5_4', 1, 1);
 
 --  テーブル quiz_game.q0022_question_show_record の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `q0022_question_show_record` (
@@ -178,7 +242,12 @@ CREATE TABLE IF NOT EXISTS `q0022_question_show_record` (
   UNIQUE KEY `q0012_id_q0021_id` (`q0012_id`,`q0021_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- エクスポートするデータが選択されていません
+-- テーブル quiz_game.q0022_question_show_record: ~4 rows (約) のデータをダンプしています
+INSERT INTO `q0022_question_show_record` (`id`, `q0012_id`, `q0021_id`, `created_at`) VALUES
+	(1, 1, 1, '2024-01-30 12:00:00'),
+	(2, 1, 2, '2024-01-30 13:00:00'),
+	(3, 1, 3, '2024-01-30 14:00:00'),
+	(4, 1, 4, '2024-01-30 15:00:00');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
